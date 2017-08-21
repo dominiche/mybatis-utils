947a2633c82a402904bc88828ca5a72973ffacf1
herongxing <herx@healthmall.cn>
2017-08-21 15:12:54 +0800

    1.1.1 fix:pageSize,pageIndex may be negative

--- README.md
+++ README.md
@@ -5,6 +5,8 @@
 
 
 版本更新日志：
+1.1.1:
+    (1)修复PageParam的pageIndex、pageSize可能被赋予负值
 1.1.0： 
     （1）UpdateFieldsUtils支持根据bean构建成UpdateFieldUnit
     （2）支持根据bean的值来更新，null值的字段忽略，非空的更新
--- src/main/java/dominic/mybatis/bean/PageParam.java
+++ src/main/java/dominic/mybatis/bean/PageParam.java
@@ -19,12 +19,34 @@ public class PageParam implements SupportUnit, Serializable{
     private Integer pageIndex = 0;
     private Integer pageSize = 10;
 
+    @Override
+    public String SQL() {
+        return " limit " + getPageStart() + "," + pageSize;
+    }
+
     public Integer getPageStart() {
         return pageIndex*pageSize;
     }
 
-    @Override
-    public String SQL() {
-        return " limit " + getPageStart() + "," + pageSize;
+    public Integer getPageIndex() {
+        return pageIndex;
+    }
+
+    public void setPageIndex(Integer pageIndex) {
+        if (null == pageIndex || pageIndex < 0) {
+            pageIndex = 0;
+        }
+        this.pageIndex = pageIndex;
+    }
+
+    public Integer getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(Integer pageSize) {
+        if (null == pageSize || pageSize < 0) {
+            pageSize = 10;
+        }
+        this.pageSize = pageSize;
     }
 }
